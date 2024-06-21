d3142fcb0aea7950099f7eecc3a9a43d2a878ba7
Andrés Choque Lopez <andreschoquelopez@gmail.com>
2024-06-20 21:52:50 -0300

    Agregar la clase LanternaMenuAcciones y se agrega una nueva dependencia.

---
+++ framework/src/main/java/choque/framework/ui/LanternaMenuAcciones.java
@@ -0,0 +1,59 @@
+package choque.framework.ui;
+
+import choque.framework.Accion;
+import choque.framework.MenuAcciones;
+import com.googlecode.lanterna.gui2.MultiWindowTextGUI;
+import com.googlecode.lanterna.gui2.WindowBasedTextGUI;
+import com.googlecode.lanterna.gui2.dialogs.ActionListDialogBuilder;
+import com.googlecode.lanterna.screen.Screen;
+import com.googlecode.lanterna.screen.TerminalScreen;
+import com.googlecode.lanterna.terminal.DefaultTerminalFactory;
+import com.googlecode.lanterna.terminal.Terminal;
+
+import java.io.IOException;
+import java.util.List;
+
+public class LanternaMenuAcciones extends MenuAcciones {
+	final WindowBasedTextGUI textGUI;
+
+	private String opcionSeleccionada = "";
+
+	public LanternaMenuAcciones() {
+		// Setup terminal and screen layers
+		Terminal terminal = null;
+		Screen screen = null;
+		try {
+			terminal = new DefaultTerminalFactory().createTerminal();
+			screen = new TerminalScreen(terminal);
+			screen.startScreen();
+		} catch (IOException e) {
+			throw new RuntimeException(e);
+		}
+
+		// Setup WindowBasedTextGUI for dialogs
+		textGUI = new MultiWindowTextGUI(screen);
+	}
+
+	@Override
+	public void mostrarMenu(List<String> items) {
+		var builder = new ActionListDialogBuilder()
+				.setTitle("Menú de acciones")
+				.setDescription("Seleccione una acción")
+				.setCanCancel(false);
+
+		// Agregar acciones
+		items.forEach((idItem) -> {
+			Accion accion = getItem(idItem).orElseThrow();
+			builder.addAction(accion.descripcionItemMenu(), () -> {
+				opcionSeleccionada = idItem;
+			});
+		});
+
+		builder.build().showDialog(textGUI);
+	}
+
+	@Override
+	public String getInputParaMenu() {
+		return opcionSeleccionada;
+	}
+}
