5a0f4fc53eea39af5d222263201cf71f7499f229
Andrés Choque Lopez <andreschoquelopez@gmail.com>
2024-06-22 19:33:37 -0300

    Usar MenuCerradoException en LanternaMenuAcciones.

--- framework/src/main/java/choque/framework/ui/LanternaMenuAcciones.java
+++ framework/src/main/java/choque/framework/ui/LanternaMenuAcciones.java
@@ -2,6 +2,7 @@ package choque.framework.ui;
 
 import choque.framework.Accion;
 import choque.framework.MenuAcciones;
+import choque.framework.MenuCerradoException;
 import com.googlecode.lanterna.gui2.MultiWindowTextGUI;
 import com.googlecode.lanterna.gui2.WindowBasedTextGUI;
 import com.googlecode.lanterna.gui2.dialogs.ActionListDialogBuilder;
@@ -16,7 +17,7 @@ import java.util.List;
 public class LanternaMenuAcciones extends MenuAcciones {
 	final WindowBasedTextGUI textGUI;
 
-	private String opcionSeleccionada = "";
+	private String opcionSeleccionada;
 
 	public LanternaMenuAcciones() {
 		// Setup terminal and screen layers
@@ -40,6 +41,7 @@ public class LanternaMenuAcciones extends MenuAcciones {
 				.setTitle("Menú de acciones")
 				.setDescription("Seleccione una acción")
 				.setCanCancel(false);
+		opcionSeleccionada = null;
 
 		// Agregar acciones
 		items.forEach((idItem) -> {
@@ -53,7 +55,8 @@ public class LanternaMenuAcciones extends MenuAcciones {
 	}
 
 	@Override
-	public String getInputParaMenu() {
+	public String getInputParaMenu() throws MenuCerradoException {
+		if (opcionSeleccionada == null) throw new MenuCerradoException();
 		return opcionSeleccionada;
 	}
 
